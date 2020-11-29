6d912503eb19a35c9af956f4cc7e473858c4ebaf
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2020-11-29 17:03:56 +0100

    Prepare for deployment to staging and production

--- src/QsCompiler/ServerRunner/Startup.cs
+++ src/QsCompiler/ServerRunner/Startup.cs
@@ -11,31 +11,27 @@ namespace ServerRunner
     [SuppressMessage("Documentation", "SA1600", Justification = "Boilerplate")]
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) { }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsStaging())
             {
-                app.UseDeveloperExceptionPage();
+                // Heroku closes connections that are inactive for more than 55 seconds
+                var webSocketOptions = new WebSocketOptions
+                {
+                    KeepAliveInterval = TimeSpan.FromSeconds(50)
+                };
+
+                app.UseWebSockets(webSocketOptions);
+            }
+            else
+            {
+                app.UseWebSockets();
             }
 
-            // Heroku closes connections that are inactive for more than 55 seconds
-            var webSocketOptions = new WebSocketOptions
-            {
-                KeepAliveInterval = TimeSpan.FromSeconds(45)
-            };
-
-            app.UseWebSockets(webSocketOptions)
-               .UseRouting()
-               .UseEndpoints(endpoints => { endpoints.MapStreamJsonRpc("/monaco-editor"); })
-               .Run(async context =>
-                {
-                    await context.Response.WriteAsync("-- You're looking for the WebSocket endpoint for the Q# Language Server on port 8091 --");
-                });
+            app.UseRouting()
+               .UseEndpoints(endpoints => { endpoints.MapStreamJsonRpc("/monaco-editor"); });
         }
     }
 }
